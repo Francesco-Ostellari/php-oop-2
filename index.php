8726b0fc238db1fb715cf8a42234434f6a60c927
Francesco-Ostellari <francescoostellari@gmail.com>
2022-02-16 17:49:55 +0100

    fix-index

--- index.php
+++ index.php
@@ -6,13 +6,12 @@
 try {
   
   $user = new User('Francesco', 'Ostellari', 'via roma RM', 123456789, "francesco@mail.it", new Card('Francesco', 'Ostellari', 1111222233334444, 123, '01/23'));
-  $product = new Product('iPhone ', 1000, 'Apple ');
+  $product = new Product('iPhone ', 1000, ' Apple ');
   var_dump($user, $product);
   
   echo $product->getName();
-  echo $product->getModel();
-
   echo $product->getPrice(true, 10);
+  echo $product->getBrand();
 
 } catch (Exception $error) {
   echo $error->getMessage();
